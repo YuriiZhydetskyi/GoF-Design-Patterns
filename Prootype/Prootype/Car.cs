5ff6c7be3f18a4a83db8183d0f453a0fb1b39d6f
z.yurii <zhidetskij@gmail.com>
2023-06-24 19:27:29 +0200

    Added Prototype pattern

---
+++ Prootype/Prootype/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace Prootype;
+
+internal class Car : IPrototype<Car>
+{
+    public CarTypeEnum CarType { get; set; }
+    public string Number { get; set; }
+    public string Color { get; set; }
+    public Car(CarTypeEnum carType, string number, string color)
+    {
+        CarType = carType;
+        Number = number;
+        Color = color;
+    }
+
+    public Car Clone()
+    {
+        return new Car(CarType, Number, Color);
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello! My name is {Number}. I'm your new {CarType}");
+    }
+}
