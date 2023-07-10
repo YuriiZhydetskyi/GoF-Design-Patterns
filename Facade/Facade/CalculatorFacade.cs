472f0da5660d8abb605784a97ca3e9ec5b9a4aa1
z.yurii <zhidetskij@gmail.com>
2023-07-10 19:11:06 +0200

    Add Facade Pattern

---
+++ Facade/Facade/CalculatorFacade.cs
@@ -0,0 +1,35 @@
+﻿namespace Facade;
+
+public class CalculatorFacade
+{
+    private CalculatorSoapClient _client;
+
+    public CalculatorFacade()
+    {
+        _client = new CalculatorSoapClient();
+    }
+
+    public int Add(int num1, int num2)
+    {
+        return _client.Add(num1, num2);
+    }
+
+    public int Subtract(int num1, int num2)
+    {
+        return _client.Subtract(num1, num2);
+    }
+
+    public int Multiply(int num1, int num2)
+    {
+        return _client.Multiply(num1, num2);
+    }
+
+    public int Divide(int num1, int num2)
+    {
+        if (num2 == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero");
+        }
+        return _client.Divide(num1, num2);
+    }
+}
---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Facade;
+
+Console.WriteLine("Hello, World!");
+
+CalculatorFacade calculator = new CalculatorFacade();
+int sum = calculator.Add(4, 5);
+int difference = calculator.Subtract(10, 3);
+int product = calculator.Multiply(4, 5);
+int quotient = calculator.Divide(20, 4);
+
+Console.WriteLine($"Sum: {sum}");
