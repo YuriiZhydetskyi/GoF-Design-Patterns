0284c83f5ff5274e093417edcccdde673931b2cd
z.yurii <zhidetskij@gmail.com>
2023-07-11 13:40:27 +0200

    Add Proxy Pattern

---
+++ Proxy/Proxy/CalculatorFacadeCache.cs
@@ -0,0 +1,56 @@
+﻿namespace Proxy;
+
+internal class CalculatorFacadeCache : ICalculatorFacade
+{
+    private CalculatorFacade _calculator;
+
+    int? AddResult { get; set; }
+    int? SubtractResult { get; set; }
+    int? MultiplyResult { get; set; }
+    int? DivideResult { get; set; }
+
+    public CalculatorFacadeCache()
+    {
+        _calculator = new CalculatorFacade();
+    }
+
+    public int Add(int num1, int num2)
+    {
+        if (AddResult == null)
+        {
+            AddResult = _calculator.Add(num1, num2);
+        }
+        
+        return AddResult.Value;
+    }
+
+    public int Divide(int num1, int num2)
+    {
+        if (DivideResult == null)
+        {
+            DivideResult = _calculator.Divide(num1, num2);
+        }
+        
+        return DivideResult.Value;
+    }
+
+    public int Multiply(int num1, int num2)
+    {
+        if (MultiplyResult == null)
+        {
+            MultiplyResult = _calculator.Multiply(num1, num2);
+        }
+        
+        return MultiplyResult.Value;
+    }
+
+    public int Subtract(int num1, int num2)
+    {
+        if (SubtractResult == null)
+        {
+            SubtractResult = _calculator.Subtract(num1, num2);
+        }
+        
+        return SubtractResult.Value;
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,14 @@
+﻿using Proxy;
+
+//ICalculatorFacade calculator = new CalculatorFacade();
+ICalculatorFacade calculator = new CalculatorFacadeCache();
+int sum = calculator.Add(4, 5);
+int difference = calculator.Subtract(10, 3);
+int product = calculator.Multiply(4, 5);
+int quotient = calculator.Divide(20, 4);
+
+Console.WriteLine($"Sum: {sum}");
+
+int sum2 = calculator.Add(4, 20);
+Console.WriteLine($"Sum: {sum2}"); 
+
