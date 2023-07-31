9775f6309804600b3a22b6a4c48a9323fa1ff8e0
z.yurii <zhidetskij@gmail.com>
2023-07-31 20:22:30 +0200

    Add Iterator Pattern

---
+++ Iterator/Iterator/Iterators/AuthorIterator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace Iterator.Iterators;
+class AuthorIterator : IEnumerator
+{
+    List<Book> bookList;
+    int position = -1;
+
+    public AuthorIterator(List<Book> list)
+    {
+        bookList = list.OrderBy(x => x.AuthorName).ToList();
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return (position < bookList.Count);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public string Current
+    {
+        get
+        {
+            try
+            {
+                return bookList[position].AuthorName;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
